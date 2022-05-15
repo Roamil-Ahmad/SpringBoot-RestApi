40e45fb47490566b6ee1b750cc9a6d604b7a4785
RomailAhmad <chromailahmad@gmail.com>
2022-05-15 15:15:28 +0500

    remove comments

--- src/main/java/com/hblmfb/bbs/CustomHealthIndicator.java
+++ src/main/java/com/hblmfb/bbs/CustomHealthIndicator.java
@@ -9,12 +9,11 @@ public class CustomHealthIndicator extends AbstractHealthIndicator {
 
 	@Override
 	protected void doHealthCheck(Builder builder) throws Exception {
-		// You can use the builder to display status details
+		
         builder.up()
                 .withDetail("Service", "Running")
                 .withDetail("Error", "No Error- Healthy status");
-        //If you add below line, it will show on the health page - "error":"java.lang.RuntimeException:
-        //.withException(new RuntimeException());
+       
 		
 	}
 
